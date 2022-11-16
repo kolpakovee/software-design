3df0c8c792fe447555905f344f87613a5d6c4e86
kolpakovee <eekolpakov@edu.hse.ru>
2022-11-16 13:57:58 +0300

    Change class Group.java

--- hw2/src/Group.java
+++ hw2/src/Group.java
@@ -7,7 +7,7 @@ public class Group {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input number of students: ");
         int studentsCount = scanner.nextInt();
-        if (studentsCount <= 0) {
+        if (studentsCount <= 0 || studentsCount > 30) {
             System.out.print("You entered the wrong number of students!");
             return false;
         }
